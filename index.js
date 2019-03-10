0a6a7157d2f886f235af549553218e8c8d2c9673
Eric Hardiman <hardimaneric@gmail.com>
2019-03-10 18:31:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file
+function findMatching(drivers, name)
+{
+  return drivers.filter(function(driver)
+  {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+}
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function fuzzyMatch(drivers, name)
+{
+  return drivers.filter(function(driver)
+  {
+    return driver.toLowerCase().slice(0,2) === name.toLowerCase().slice(0,2);
+  });
+}
+
+function matchName(drivers, name)
+{
+  return drivers.filter(function(driver)
+  {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
+
+// const drivers = [
+//   {
+//     name: 'Bobby',
+//     hometown: 'Pittsburgh' },
+//   {
+//     name: 'Sammy',
+//     hometown: 'New York' } ,
+//   {
+//     name: 'Sally',
+//     hometown: 'Cleveland' },
+//   {
+//     name: 'Annette',
+//     hometown: 'Los Angeles' },
+//   {
+//     name: 'Bobby',
+//     hometown: 'Tampa Bay' }
+// ];
