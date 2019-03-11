fb96fa149cb04b8d31cc2175967b72f6b7778bd0
Ethan Nam <ethan.s.nam@gmail.com>
2019-03-11 14:18:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file
+function findMatching(drivers, expression) {
+  return drivers.filter(function (name) { return name.match(new RegExp(expression, "i")); });
+}
+
+function fuzzyMatch(drivers, expression) {
+  return drivers.filter(function (name) { 
+    regex = new RegExp(`^${expression}`, "i");
+    if (regex.test(name)) {
+      return name;
+    } 
+  });
+}
+
+function matchName(drivers, expression) {
+  return drivers.filter(function (object) {
+    return object.name === expression;
+  })
+}
