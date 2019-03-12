52b6ca68b926bbae1dafc9e1e073ea0b664407df
bdix1111 <brianna_dixon@rocketmail.com>
2019-03-12 10:26:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching (drivers, string) {
+  return drivers.filter(function(driver) {
+    return driver.toLowerCase() === string.toLowerCase();
+  });
+}
+
+function fuzzyMatch (drivers, string) {
+  let nameLength = string.length;
+  return drivers.filter(function (driver) {
+    return driver.slice(0, nameLength) === string;
+  });
+}
+
+function matchName (drivers, string) {
+  return drivers.filter(function(driver) {
+    return driver.name.toLowerCase() === string.toLowerCase();
+  });
+}
